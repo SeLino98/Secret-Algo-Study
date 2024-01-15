f47c679c391290d8bff49a65177aa770f6466445
KiMi_No.98 <42768156+SeLino98@users.noreply.github.com>
2024-01-15 12:20:27 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 184 ms, Memory: 18100 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Position implements Comparable<Position>{
+    int curPos ;
+    int cost;
+    public Position(int curPos, int cost) {
+        this.curPos = curPos;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Position o) {
+        return this.cost - o.cost;
+    }
+}
+public class Main {
+    static int startPos,endPos;
+
+    final static int MaxPos = 100_001;
+    static boolean [] isVisited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        //N점 K점
+        isVisited = new boolean[MaxPos];
+
+        startPos = Integer.parseInt(st.nextToken());
+        endPos  = Integer.parseInt(st.nextToken());
+        int answer = BFS();
+        System.out.println(answer);
+    }
+    public static int BFS(){
+        //큐에 넣고
+        PriorityQueue<Position> node = new PriorityQueue<>();
+        node.add(new Position(startPos,0));
+        while (!node.isEmpty()){
+            Position curNode = node.poll();
+            if (curNode.curPos == endPos){//endPos에 도착하면 종료
+//                System.out.println("ASDF");
+                return curNode.cost;
+            }
+            if (curNode.curPos<MaxPos){
+                if (!isVisited[curNode.curPos]){
+
+                    isVisited[curNode.curPos] = true;
+//                    System.out.println(curNode.curPos+"_CURPOS");
+                    if (curNode.curPos<2*endPos- startPos){
+                        //*2일 때
+                        //curPos < 2endPos - startPos
+//                    System.out.print("A ");
+                        node.add(new Position(curNode.curPos*2, curNode.cost));
+                    }
+                    if (curNode.curPos>0){//-1일 때
+//                    System.out.print("B ");
+                        node.add(new Position(curNode.curPos-1, curNode.cost+1));
+                    }
+                    if (curNode.curPos<endPos){//+1일 때
+//                    System.out.print("C ");
+                        node.add(new Position(curNode.curPos+1, curNode.cost+1));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}

055712ada154a49525a9aea5fa9499cc1063cdf6
Ki_Mi_No <42768156+SeLino98@users.noreply.github.com>
2024-02-16 09:53:59 +0900

    [Bronze II] Title: 공 바꾸기, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Bronze II/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int [] list = new int[n];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = i+1;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int first = Integer.parseInt(st.nextToken())-1;
+            int second =Integer.parseInt(st.nextToken())-1;
+            int tmp = list[first];
+            list[first] = list[second];
+            list[second] = tmp;
+        }
+
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i]+" ");
+        }
+
+
+    }
+}

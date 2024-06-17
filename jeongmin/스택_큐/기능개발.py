8c2d185e3200fde8e1cc373c0c25a1dec0057b18
이정민 <bona3377@naver.com>
2024-06-18 04:27:25 +0900

    기능개발_큐

---
+++ jeongmin/스택_큐/기능개발.py
@@ -0,0 +1,25 @@
+# progresses : 배포되어야 하는 순서대로의 작업
+# speeds : 각 작업의 개발 속도
+# 진도 100되면 배포 가능, 근데 앞의 것부터 배포된다.
+# 각 배포당 몇 개의 기능이 배포되는가?
+
+from collections import deque
+
+def solution(progresses, speeds):
+    progresses = deque(progresses)
+    answer = []
+    while progresses:
+        count = 0
+        for i in range(len(progresses)):
+            if progresses[i] < 100:
+                if progresses[i] + speeds[i] >= 100:
+                    progresses[i] = 100
+                else:
+                    progresses[i] += speeds[i]
+        if progresses[0] == 100:
+            while progresses and progresses[0] == 100:
+                progresses.popleft()
+                count += 1
+                if len(progresses) == 0 or progresses[0] != 100:
+                    answer.append(count)
+    return answer

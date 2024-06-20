b7a9be8a0bc432aed74f4a96620241d6a27d8726
이정민 <bona3377@naver.com>
2024-06-20 17:00:28 +0900

    프로세스_스택/큐

---
+++ jeongmin/스택_큐/프로세스.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(priorities, location):
+    pri_que = deque([(priorities[i], i) for i in range(len(priorities))])
+    find_process = (priorities[location], location)
+    max_process = deque(sorted(priorities, reverse=True))
+    count = 0
+    while pri_que:
+        process = pri_que.popleft()
+        if max_process[0] == process[0]:
+            max_process.popleft()
+            count += 1
+            if process == find_process:
+                return count
+        pri_que.append(process)
+
+
+print(solution([2, 1, 3, 2], 2))
+print(solution([1, 1, 9, 1, 1, 1], 0))

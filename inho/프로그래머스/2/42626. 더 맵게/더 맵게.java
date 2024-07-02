922f87440dcdbc0fc7540ccdc89fcd0944a78cbc
김인호 <linos98@naver.com>
2024-07-02 14:41:09 +0900

    solved 디스크 컨트롤러

--- inho/프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ inho/프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,18 +2,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        //백만이면,,
+        //최소 힙 문제 
         PriorityQueue<Integer> q = new PriorityQueue<>();
         for(int token : scoville){
             q.add(token);   
         }
-        int playTime = 0;
-        int tmp = 0;
+        int playTime = 0; //정답 출력용
+        int tmp = 0; //임시 값 
         boolean canAnswer = false;
-        tmp = q.peek();    //현재 큐에서 작은 놈을 넣는다.
-            // System.out.println(tmp+"::"+playTime);
+        tmp = q.peek();    //현재 큐에서 작은 놈을 찾는다.
         if(K<=tmp){
-                canAnswer = true;
+                canAnswer = true; //첫빵에 나올 수 있으니 함 거르고 시작
         }else{
             while(q.size()>1){
             playTime ++;
@@ -22,7 +21,6 @@ class Solution {
             int newScoville = firstScoville + (secondScoville*2);
             q.add(newScoville);
              tmp = q.peek();    //현재 큐에서 작은 놈을 넣는다.
-            // System.out.println(tmp+"::"+playTime);
             if(K<=tmp){
                 canAnswer = true;
                 break;

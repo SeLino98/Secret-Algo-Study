ee65a8125c55f3fc608f168b1d270b29c967b229
이정민 <bona3377@naver.com>
2024-07-09 02:54:56 +0900

    카펫_완전탐색

--- jeongmin/완전탐색/카펫_완탐_240709.py
+++ jeongmin/완전탐색/카펫_완탐_240709.py
@@ -2,7 +2,7 @@
 # 노란색은 (가로-2)*(세로-2) = 약수조합으로 구해도 된다
 
 def solution(brown, yellow):
-    for i in range(brown//2, 0, -1):  # brown에서 반만 확인('가로+세로-2'의 값 범위니까)
+    for i in range(brown//2, 2, -1):  # brown에서 반만 확인('가로+세로-2'의 값 범위니까)
         if yellow % (i-2) == 0:  # i가 노란색의 약수면
             j = (yellow // (i-2)) + 2  # 약수의 대칭값 찾아서
             if (i+j-2)*2 == brown:  # brown이랑도 부합하는지 확인 후

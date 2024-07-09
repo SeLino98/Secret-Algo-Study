df3e355e9ab9b9eb4477ec772bfb9416f5d8162c
김수빈 <ksb143@naver.com>
2024-07-09 22:03:23 +0900

    Solved: 최대공약수와 최소공배수

--- subin/baekjoon/240709/2609.최대공약수와_최소공배수.js
+++ subin/baekjoon/240709/2609.최대공약수와_최소공배수.js
@@ -1,6 +1,29 @@
 const fs = require('fs')
 const io = process.platform === "linux" ? "/dev/stdin" : './input.txt';
 
-const [x, y] = fs.readFileSync(io).toString().trim().split(' ').map(Number)
+const [a, b] = fs.readFileSync(io).toString().trim().split(' ').map(Number)
 
-console.log(x, y)
+// 유클리드 호제법
+const value = [a, b]
+value.sort((x, y) => {
+  return x - y
+})
+let smallValue = value[0]
+let bigValue = value[1]
+while (true) {
+  let reminder = bigValue % smallValue
+  if (reminder === 0) {
+    break
+  }
+  bigValue = smallValue
+  smallValue = reminder
+}
+
+// 최대공약수
+const gcd = smallValue
+// 최소공배수
+const lcm = a * b / smallValue 
+
+
+console.log(gcd)
+console.log(lcm)

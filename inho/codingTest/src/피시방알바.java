e9777d6795d3c1d4a615611aad6b54420d0a86d1
김인호 <linos98@naver.com>
2024-09-19 23:15:18 +0900

    [feat] solved bakjoon

---
+++ inho/codingTest/src/피시방알바.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 피시방알바 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int personNumber = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        boolean [] isChecked = new boolean[101];
+        int answer = 0;
+        for (int i = 0; i < personNumber; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            if (isChecked[tmp]){
+                answer++;
+            }else{
+                isChecked[tmp] = true;
+            }
+        }
+        System.out.println(answer);
+    }
+
+}

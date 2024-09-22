e37f2ded65f4a294da78677f8d7eb29fc147b463
김인호 <linos98@naver.com>
2024-09-22 23:43:02 +0900

    [feat] solved bakjoon

---
+++ inho/codingTest/src/출석번호.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class 출석번호 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean [] list = new boolean[31];
+        for (int t = 0; t < 28; t++) {
+            int donePerson = Integer.parseInt(br.readLine());
+            list[donePerson] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < list.length; i++) {
+            if (!list[i]) sb.append(i).append('\n');
+        }
+        System.out.print(sb);
+    }
+}

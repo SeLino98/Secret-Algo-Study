b8a33379bf2bd56a3a9c4d83f6dc25b3e2c76f1e
Ki_Mi_No <42768156+SeLino98@users.noreply.github.com>
2024-10-08 19:08:23 +0900

    [Silver I] Title: 현수막, Time: 188 ms, Memory: 19900 KB -BaekjoonHub

--- 백준/Silver/14716. 현수막/현수막.java
+++ 백준/Silver/14716. 현수막/현수막.java
@@ -5,21 +5,16 @@ public class Main {
     static int [] dx ={-1,0,1,0,-1,1,-1,1};
     static int [] dy ={0,1,0,-1,1,1,-1,-1};
     static int [][]map ;
-    static boolean[][] isVisited ;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         rowSize = Integer.parseInt(st.nextToken());
         colSize = Integer.parseInt(st.nextToken());
         map = new int[rowSize][colSize];
-        isVisited = new boolean[rowSize][colSize];
-        for (int i = 0; i < rowSize ; i++) {
+         for (int i = 0; i < rowSize ; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < colSize; j++) {
                 int tmp = Integer.parseInt(st.nextToken());
-                if (tmp==1){
-                    isVisited[i][j] = true;
-                }
                 map[i][j] = tmp;
             }
         }
@@ -27,17 +22,15 @@ public class Main {
         for (int i = 0; i < rowSize ; i++) {
             for (int j = 0; j < colSize; j++) {
                 if(map[i][j] == 1){
-                   if (isVisited[i][j]){
-                       BFS(i,j);
-                       answer++;
-                   }
+                    BFS(i,j);
+                    answer++;
                 }
             }
         }
-        System.out.println(answer);
+        System.out.print(answer);
     }
     static void BFS(int y, int x){
-        Queue<int[]> q = new ArrayDeque<>();
+        ArrayDeque<int[]> q = new ArrayDeque<>();
         q.add(new int[]{y,x});
         while (!q.isEmpty()){
             int []posXY = q.poll();
@@ -47,12 +40,13 @@ public class Main {
                 int nX = posX + dx[i];
                 int nY = posY + dy[i];
                 if (nY>=0&&nX>=0&&nY<rowSize&&nX<colSize){
-                    if (isVisited[nY][nX] && map[nY][nX]==1){
+                    if (map[nY][nX]==1){
                         q.add(new int[]{nY,nX});
-                        isVisited[nY][nX] = false;
+                        map[nY][nX] = 2;
                     }
                 }
             }
         }
     }
+
 }

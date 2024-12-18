5d5dd4dc60540d8f5c1338c2a5fa6d95c7896b64
Ki_Mi_No <42768156+SeLino98@users.noreply.github.com>
2024-12-18 14:02:39 +0900

    [Gold V] Title: 수 고르기, Time: 272 ms, Memory: 28888 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        int listSize, findValue ;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        listSize = Integer.parseInt(st.nextToken());
+        findValue = Integer.parseInt(st.nextToken());
+        int [] list = new int[listSize];//1 5 3
+        for (int i = 0; i < listSize; i++) {
+            list[i] = Integer.parseInt(br.readLine());
+        }
+        if (list.length== 1){
+            System.out.print(0);
+            return;
+        }
+        Arrays.sort(list);
+        int lt = 0;
+        int rt = 1;
+        int answer = Integer.MAX_VALUE;
+        while (rt < listSize &&lt<rt){
+            int tmp = list[rt] - list[lt];
+            if (tmp>=findValue){
+                answer = Math.min(answer,tmp);
+                lt++;
+            }else{
+                rt++;
+            }
+            if (lt == rt) rt++;
+        }
+        System.out.print(answer);
+    }
+}

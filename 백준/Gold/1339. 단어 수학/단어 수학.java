a30f2574a10c80735260d1f36562c0ccbcceff3d
Ki_Mi_No <42768156+SeLino98@users.noreply.github.com>
2024-12-31 17:43:21 +0900

    [Gold IV] Title: 단어 수학, Time: 184 ms, Memory: 18384 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,44 +1,52 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int wordSize = Integer.parseInt(br.readLine());
-        String[] list = new String[wordSize];
-        Map<Character, Long> weightMap = new HashMap<>();
+        int n = Integer.parseInt(br.readLine());
+        String[] words = new String[n];
+
+        // 각 단어 입력
+        for (int i = 0; i < n; i++) {
+            words[i] = br.readLine();
+        }
+
+        // 가중치 저장용 Map
+        Map<Character, Integer> weightMap = new HashMap<>();
 
         // 각 문자에 대한 가중치 계산
-        for (int i = 0; i < wordSize; i++) {
-            String tmp = br.readLine();
-            list[i] = tmp;
-            int power = tmp.length() - 1;
-            for (char token : tmp.toCharArray()) {
-                weightMap.put(token, weightMap.getOrDefault(token, 0L) + (long) Math.pow(10, power));
+        for (String word : words) {
+            int power = word.length() - 1; // 자릿수 계산
+            for (char ch : word.toCharArray()) {
+                weightMap.put(ch, weightMap.getOrDefault(ch, 0) + (int) Math.pow(10, power));
                 power--;
             }
         }
 
         // 가중치 기준으로 문자 정렬
-        List<Map.Entry<Character, Long>> sortedEntries = new ArrayList<>(weightMap.entrySet());
-        sortedEntries.sort((o1, o2) -> Long.compare(o2.getValue(), o1.getValue()));
+        List<Map.Entry<Character, Integer>> sortedEntries = new ArrayList<>(weightMap.entrySet());
+        sortedEntries.sort((e1, e2) -> e2.getValue() - e1.getValue()); // 내림차순 정렬
 
         // 숫자 배정
         int value = 9;
-        Map<Character, Integer> charToNumber = new LinkedHashMap<>();
-        for (Map.Entry<Character, Long> tokenMap : sortedEntries) {
-            charToNumber.put(tokenMap.getKey(), value--);
+        Map<Character, Integer> charToNumber = new HashMap<>();
+        for (Map.Entry<Character, Integer> entry : sortedEntries) {
+            charToNumber.put(entry.getKey(), value--);
         }
 
         // 합산 계산
-        long answer = 0;
-        for (int i = 0; i < wordSize; i++) {
-            long tmpValue = 0;
-            for (char s : list[i].toCharArray()) {
-                tmpValue = tmpValue * 10 + charToNumber.get(s);
+        int totalSum = 0;
+        for (String word : words) {
+            int wordValue = 0;
+            for (char ch : word.toCharArray()) {
+                wordValue = wordValue * 10 + charToNumber.get(ch);
             }
-            answer += tmpValue;
+            totalSum += wordValue;
         }
-        System.out.println(answer);
+
+        // 결과 출력
+        System.out.println(totalSum);
     }
 }

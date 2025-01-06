fc53b31a3b4379cb2bb88304537d887fcdd64eb3
Ki_Mi_No <42768156+SeLino98@users.noreply.github.com>
2025-01-06 14:19:59 +0900

    [level 2] Title: [1차] 캐시, Time: 40.70 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,32 +1,26 @@
 import java.util.*;
-class Solution { //프로그래머스 [1차]캐시 카카오 블라인드
+class Solution { 
     public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
         if (cacheSize==0) return cities.length*5;
+
+        int answer = 0;
         Deque<String> deque = new ArrayDeque<>();
+
         for (int i = 0; i < cities.length; i++) {
             String tmpName = cities[i].toLowerCase();
-            if (deque.size()<cacheSize){//큐가 캐시사이즈값보다 작은 경우
-                if (deque.contains(tmpName)){//새로 들어온 값을 이미 갖고 있는 경우
-                    deque.remove(tmpName);// 그 값을 제거.
-                    deque.offerLast(tmpName); //
-                    answer+=1;
-                }else{
-                    deque.offerLast(tmpName);
-                    answer += 5;
-                }
 
-            }else{
-                if (deque.contains(tmpName)){//값이 있는 경우
-                    deque.remove(tmpName);// 그 값을 제거.
-                    deque.offerLast(tmpName); //
-                    answer+=1;
-                }else{//값이 없는 경우
+            if (deque.contains(tmpName)){//값이 있는 경우
+                //캐시 hit
+                deque.remove(tmpName);// 그 값을 제거.
+                answer+=1;
+            }else{//값이 없는 경우
+                //캐시 미스
+                if (deque.size()==cacheSize){
                     deque.pollFirst();
-                    deque.offerLast(tmpName);
-                    answer+=5;
                 }
+                answer+=5;
             }
+            deque.offerLast(tmpName);
         }
         return answer;
     }

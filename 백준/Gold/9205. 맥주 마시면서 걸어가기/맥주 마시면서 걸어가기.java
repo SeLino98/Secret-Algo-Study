fec0833d730b025fa196fc9d93b76d694f14e0f2
Ki_Mi_No <42768156+SeLino98@users.noreply.github.com>
2025-01-27 14:28:38 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 80 ms, Memory: 12504 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,61 +1,110 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st ;
-    static StringBuilder sb = new StringBuilder();
-    static int []startXY = new int[2];
-    static int []endXY = new int[2];
-    static int [][] storeXY ;
-    static int storeSize ;
-    static boolean [] isVisitedStoreList;
-    public static void main(String[] args) throws IOException{
-        int testCase = Integer.parseInt(br.readLine());
+
+    public static void main(String[] args) throws IOException {
+        InputHandler inputHandler = new InputHandler();
+        OutputHandler outputHandler = new OutputHandler();
+        BFSHandler bfsHandler = new BFSHandler();
+
+        int testCase = inputHandler.readTestCaseCount();
         for (int t = 0; t < testCase; t++) {
-            inputAndSettingData();
-            outputStringBuilder();
+            TestCaseData data = inputHandler.readTestCaseData();
+            boolean result = bfsHandler.canReachEnd(data);
+            outputHandler.appendResult(result);
         }
+        outputHandler.printResults();
+    }
+}
+
+// Input 처리 클래스
+class InputHandler {
+    private final BufferedReader br;
+
+    public InputHandler() {
+        this.br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public int readTestCaseCount() throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+
+    public TestCaseData readTestCaseData() throws IOException {
+        int storeSize = Integer.parseInt(br.readLine());
+        int[] startXY = readCoordinates();
+        int[][] storeXY = new int[storeSize][2];
+        for (int i = 0; i < storeSize; i++) {
+            storeXY[i] = readCoordinates();
+        }
+        int[] endXY = readCoordinates();
+        return new TestCaseData(startXY, endXY, storeXY);
+    }
+
+    private int[] readCoordinates() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        return new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+    }
+}
+
+// Output 처리 클래스
+class OutputHandler {
+    private final StringBuilder sb;
+
+    public OutputHandler() {
+        this.sb = new StringBuilder();
+    }
+
+    public void appendResult(boolean result) {
+        sb.append(result ? "happy" : "sad").append('\n');
+    }
+
+    public void printResults() {
         System.out.print(sb);
     }
-    static boolean BFS(int [] startXY, int storeSize){
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{startXY[0],startXY[1]});
-        while (!q.isEmpty()){
-            int []posXY = q.poll();
-            if (manhattanDistance(endXY,posXY)){
+}
+
+// BFS 탐색 로직 처리 클래스
+class BFSHandler {
+    public boolean canReachEnd(TestCaseData data) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[data.storeXY.length];
+
+        queue.add(data.startXY);
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            
+            // 도착지와의 거리 계산
+            if (manhattanDistance(current, data.endXY) <= 1000) {
                 return true;
             }
-            for (int i = 0; i < storeSize; i++) {
-                if (isVisitedStoreList[i]) continue;
-                int [] nXY = storeXY[i];
-                if (manhattanDistance(nXY,posXY)){
-                    isVisitedStoreList[i] = true;
-                    q.add(new int[]{nXY[0],nXY[1]});
+
+            for (int i = 0; i < data.storeXY.length; i++) {
+                if (visited[i]) continue;
+                if (manhattanDistance(current, data.storeXY[i]) <= 1000) {
+                    visited[i] = true;
+                    queue.add(data.storeXY[i]);
                 }
             }
         }
+
         return false;
     }
-    static void inputAndSettingData() throws IOException{
-        storeSize = Integer.parseInt(br.readLine());
-        storeXY = new int[storeSize][2];
-        st = new StringTokenizer(br.readLine());
-        startXY[0] = Integer.parseInt(st.nextToken());
-        startXY[1] = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < storeSize; i++) {
-            st = new StringTokenizer(br.readLine());
-            storeXY[i][0] = Integer.parseInt(st.nextToken());
-            storeXY[i][1] = Integer.parseInt(st.nextToken());
-        }
-        st = new StringTokenizer(br.readLine());
-        endXY[0] = Integer.parseInt(st.nextToken());
-        endXY[1] = Integer.parseInt(st.nextToken());
-        isVisitedStoreList = new boolean[storeSize];
+
+    private int manhattanDistance(int[] point1, int[] point2) {
+        return Math.abs(point1[0] - point2[0]) + Math.abs(point1[1] - point2[1]);
     }
-    static void outputStringBuilder(){
-        sb.append(BFS(startXY,storeSize) ? "happy": "sad").append('\n');
-    }
-    static boolean manhattanDistance(int[] point1, int[] point2) {
-        return Math.abs(point1[0] - point2[0]) + Math.abs(point1[1] - point2[1])<=1000;
+}
+
+// 테스트 케이스 데이터 클래스
+class TestCaseData {
+    public final int[] startXY;
+    public final int[] endXY;
+    public final int[][] storeXY;
+
+    public TestCaseData(int[] startXY, int[] endXY, int[][] storeXY) {
+        this.startXY = startXY;
+        this.endXY = endXY;
+        this.storeXY = storeXY;
     }
 }

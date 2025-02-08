ecd6590055742b481a1f3f49a4519a71d108bbe2
Ki_Mi_No <42768156+SeLino98@users.noreply.github.com>
2025-02-08 22:25:27 +0900

    [Silver V] Title: 문서 검색, Time: 68 ms, Memory: 11452 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        
+        char [] wordList = br.readLine().toCharArray();
+        char [] findWord = br.readLine().toCharArray();
+        int pos = 0;
+        int answer = 0;
+        int range = wordList.length - findWord.length;
+        while (pos <= range){
+            boolean isFlag = true;
+            int curPos = pos;
+            for (int j = 0; j < findWord.length; j++) {
+                if (wordList[curPos] == findWord[j]){
+                    curPos++;
+                }else{
+                    isFlag = false;
+                    break;
+                }
+            }
+            if (isFlag){
+                pos = curPos;
+                answer++;
+            }else{
+                pos++;
+            }
+        }
+        System.out.print(answer);
+    }
+}

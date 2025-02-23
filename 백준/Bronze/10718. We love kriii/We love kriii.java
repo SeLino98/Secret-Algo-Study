cfff12aa7eb4c0ba062a9b5f1508163c849a65ba
Ki_Mi_No <42768156+SeLino98@users.noreply.github.com>
2025-02-23 19:07:55 +0900

    [Bronze V] Title: We love kriii, Time: 60 ms, Memory: 11416 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args)throws IOException {
+        System.out.print("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+}

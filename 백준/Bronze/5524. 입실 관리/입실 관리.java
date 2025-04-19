51ffbd9d3a5e21ea176f1e47be2a19810ebac586
Ki_Mi_No <42768156+SeLino98@users.noreply.github.com>
2025-04-19 21:26:20 +0900

    [Bronze IV] Title: 입실 관리, Time: 68 ms, Memory: 11508 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException{
+        int testCase = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= testCase; t++) {
+            String tmp = br.readLine();
+            sb.append(tmp.toLowerCase()).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
